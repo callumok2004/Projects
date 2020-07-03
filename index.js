e92f49d788c38e88b8dee12931461badf3c61fbc
Callum OKane <35779365+callumok2004@users.noreply.github.com>
2020-07-03 22:34:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ class API {
     constructor(config) {
         this.config = config;
         this.weather = new WeatherAPI(app);
-        app.listen(config.port, () => console.log(`API Started on port ${config.port}`));
+        app.
+        listen(config.port, () => console.log(`API Started on port ${config.port}`));
     };
 };
 
 module.exports = new API(config);
+
+//GITHUB+DISCORD AUTO RESET THIS TOKEN THANKS // NTQ4MTkzODYxMDk3MjI2MjQz.Xm5-Mg.XaLwVCrV__-TD2qFQTdum9-Dm-4
